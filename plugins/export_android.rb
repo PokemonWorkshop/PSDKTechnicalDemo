0e0ea14081709dfa9d49c784d404b1e6405168cf
Scorbutics <brice.bulgarelli@gmail.com>
2026-03-20 11:25:58 +1000

    feat: encrypt the archive

--- plugins/export_android.rb
+++ plugins/export_android.rb
@@ -1,29 +1,71 @@
-# Create the PSDK Source Archive
+# Create the encrypted PSDK Source Archive for Android
 # Needs the gem 'rubyzip'
 
 require 'zip'
+require 'openssl'
+require 'tempfile'
 
-def zip_all_recursive(zipfile, path)
-  path = "#{path}/**"
+ENCRYPTION_KEY = ['1f24dd020fb077983c537dd29af01b9188406ce835bca75567b54db9be9f83f9'].pack('H*')
+EPSA_MAGIC = 'PSAE'
+EPSA_VERSION = 1
 
-  Dir.glob(path).each do |file_or_dir|
-    if File.directory? (file_or_dir)
-      zip_all_recursive(zipfile, file_or_dir)
-    else
-      puts "Adding #{file_or_dir}"
-      zipfile.add(file_or_dir, file_or_dir)
-    end
-  end
-
+unless ENCRYPTION_KEY.bytesize == 32
+  STDERR.puts "ENCRYPTION_KEY must be exactly 32 bytes (got #{ENCRYPTION_KEY.bytesize})"
+  exit 1
 end
 
 folders = ['graphics', 'Fonts', 'Data', 'audio', 'pokemonsdk', 'scripts']
 folders = folders.concat(["Saves"]) if ARGV.include?('--with_saves')
 
-ARCHIVE_NAME = (ARGV.find { |arg| !arg.start_with?('--') } || "project") + ".psa"
-File.delete(ARCHIVE_NAME) if File.exist? ARCHIVE_NAME
+no_encrypt = ARGV.include?('--no-encrypt')
+BASE_NAME = ARGV.find { |arg| !arg.start_with?('--') } || "project"
 
-Zip::File.open(ARCHIVE_NAME, create: true) do |zipfile|
-  zip_all_recursive(zipfile, '{' + folders.join(',') + '}')
-  zipfile.add("Game.rb", "Game.rb")
+# Build ZIP to a temp file (proper format with central directory, required by PhysFS)
+tmp_zip = Tempfile.new(['psdk_archive', '.zip'], binmode: true)
+begin
+  tmp_zip.close
+  Zip::File.open(tmp_zip.path, create: true) do |zip|
+    glob_pattern = '{' + folders.join(',') + '}/**/*'
+    Dir.glob(glob_pattern).each do |file_or_dir|
+      next if File.directory?(file_or_dir)
+      puts "Adding #{file_or_dir}"
+      zip.get_output_stream(file_or_dir) { |out| out.write(File.binread(file_or_dir)) }
+    end
+    puts "Adding Game.rb"
+    zip.get_output_stream("Game.rb") { |out| out.write(File.binread("Game.rb")) }
+  end
+  zip_data = File.binread(tmp_zip.path)
+
+  # Write plain .psa archive
+  psa_name = "#{BASE_NAME}.psa"
+  File.delete(psa_name) if File.exist?(psa_name)
+  File.binwrite(psa_name, zip_data)
+  puts "Plain archive written to #{psa_name} (#{File.size(psa_name)} bytes)"
+
+  unless no_encrypt
+    # Encrypt the ZIP data with AES-256-CBC
+    begin
+      cipher = OpenSSL::Cipher::AES256.new(:CBC)
+      cipher.encrypt
+      cipher.key = ENCRYPTION_KEY
+      iv = cipher.random_iv
+      encrypted_data = cipher.update(zip_data) + cipher.final
+    rescue OpenSSL::Cipher::CipherError => e
+      STDERR.puts "Encryption failed: #{e.message}"
+      exit 1
+    end
+
+    # Write the encrypted archive with header
+    epsa_name = "#{BASE_NAME}.epsa"
+    File.delete(epsa_name) if File.exist?(epsa_name)
+    File.open(epsa_name, 'wb') do |f|
+      f.write(EPSA_MAGIC)                  # 4 bytes: magic
+      f.write([EPSA_VERSION].pack('V'))    # 4 bytes: version (uint32 LE)
+      f.write(iv)                          # 16 bytes: IV
+      f.write(encrypted_data)              # rest: ciphertext
+    end
+    puts "Encrypted archive written to #{epsa_name} (#{File.size(epsa_name)} bytes)"
+  end
+ensure
+  tmp_zip.unlink
 end
